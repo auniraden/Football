34fb3036725eab5d97bb7aca23599285eb566da4
auniraden <radennn.auni@gmail.com>
2025-11-03 17:13:01 +0800

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Auni Raden on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
